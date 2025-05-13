2e8e67273ee5b50470895254af9e2939205e73ce
Maxim Efimov <efimov.mu@ya.ru>
2025-05-13 17:33:15 +0300

    Изменение проекта под Задание 1 Урока 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,29 @@
-import logo from './assets/logo.svg'
+// import logo from './assets/logo.svg'
 import './App.css'
-import { createElement } from 'react'
 
-const currentYear = new Date().getFullYear() //декларативный
 export const App = () => {
-	return createElement(
-		'div',
-		{ className: 'App' },
-		createElement(
-			'header',
-			{ className: 'App-header' },
-			createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
-			createElement('p', null, 'Edit', createElement('code', null, ` src/App.js `), ` and save to reload.`),
-			createElement(
-				'a',
-				{
-					className: 'App-link',
-					href: 'https://reactjs.org',
-					target: '_blank',
-					rel: 'noopener noreferrer',
-				},
-				'Learn React',
-			),
-			createElement('p', null, `${currentYear}`),
-		),
+	return (
+		<>
+			<div class="app">
+				<h1 class="page-heading">Ввод значения</h1>
+				<p class="no-margin-text">
+					Текущее значение <code>value</code>: "<output class="current-value"></output>"
+				</p>
+				<div class="error">Введенное значение должно содержать минимум 3 символа</div>
+				<div class="buttons-container">
+					<button class="button">Ввести новое</button>
+					<button class="button" disabled>
+						Добавить в список
+					</button>
+				</div>
+				<div class="list-container">
+					<h2 class="list-heading">Список:</h2>
+					<p class="no-margin-text">Нет добавленных элементов</p>
+					<ul class="list">
+						<li class="list-item">Первый элемент</li>
+					</ul>
+				</div>
+			</div>
+		</>
 	)
 }
