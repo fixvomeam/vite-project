9be694ee709d80d71097f88d2f6e0616d38c6026
Maxim Efimov <efimov.mu@ya.ru>
2025-05-13 17:50:18 +0300

    Перенес верстку в React-компонент.

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import './App.css'
+import styles from './app.module.css'
 
 export const App = () => {
 	return (
 		<>
-			<div className="app">
-				<h1 className="page-heading">Ввод значения</h1>
-				<p className="no-margin-text">
-					Текущее значение <code>value</code>: "<output className="current-value"></output>"
+			<div className={styles.app}>
+				<h1 className={styles[`page - heading`]}>Ввод значения</h1>
+				<p className={styles[`no-margin-text`]}>
+					Текущее значение <code>value</code>: "<output className={styles[`current-value`]}></output>"
 				</p>
-				<div className="error">Введенное значение должно содержать минимум 3 символа</div>
-				<div className="buttons-container">
-					<button className="button">Ввести новое</button>
-					<button className="button" disabled>
+				<div className={styles.error}>Введенное значение должно содержать минимум 3 символа</div>
+				<div className={styles[`buttons-container`]}>
+					<button className={styles.button}>Ввести новое</button>
+					<button className={styles.button} disabled>
 						Добавить в список
 					</button>
 				</div>
-				<div className="list-container">
-					<h2 className="list-heading">Список:</h2>
-					<p className="no-margin-text">Нет добавленных элементов</p>
-					<ul className="list">
-						<li className="list-item">Первый элемент</li>
+				<div className={styles[`list-container`]}>
+					<h2 className={styles[`list-heading`]}>Список:</h2>
+					<p className={styles[`no-margin-text`]}>Нет добавленных элементов</p>
+					<ul className={styles.list}>
+						<li className={styles[`list-item`]}>Первый элемент</li>
 					</ul>
 				</div>
 			</div>
